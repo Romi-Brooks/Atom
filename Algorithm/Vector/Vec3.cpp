de020d9debe99e4d92107050d45bc33693c966c2
Romi-Brooks <qq1694821929@gmail.com>
2026-08-14 17:29:04 +0800

    feat(math): add Vec3 type

---
+++ Algorithm/Vector/Vec3.cpp
@@ -0,0 +1,36 @@
+/**
+  * @file           : Vec3.cpp
+  * @author         : Romi Brooks
+  * @brief          :
+  * @attention      :
+  * @date           : 2026/8/14
+  Copyright (c) 2026 Romi Brooks, All rights reserved.
+**/
+
+#include "Vec3.hpp"
+
+namespace atom {
+    auto Vec3::GetX() const -> float {
+        return x_;
+    }
+
+    auto Vec3::GetY() const -> float {
+        return y_;
+    }
+
+    auto Vec3::GetZ() const -> float {
+        return z_;
+    }
+
+    auto Vec3::SetX(const float x) -> void {
+        x_ = x;
+    }
+
+    auto Vec3::SetY(const float y) -> void {
+        y_ = y;
+    }
+
+    auto Vec3::SetZ(const float z) -> void {
+        z_ = z;
+    }
+}
